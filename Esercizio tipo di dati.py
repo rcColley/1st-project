0c32cce9bc575c698acadf5d15f9086b7b800e8b
rcColley <rikhy871@gmail.com>
2025-10-21 19:22:56 +0200

    Add files via upload

---
+++ Esercizio tipo di dati.py
@@ -0,0 +1,19 @@
+x=7
+y=3
+somma=(x+y)
+print(somma)
+
+prezzo=19.99
+quantità=3
+totale=(prezzo*quantità)
+print(totale)
+
+numero=10
+x=5
+print (numero > x)
+print (numero < x)
+
+a=5
+b=2.5
+somma=(a + b)
+print (somma)
